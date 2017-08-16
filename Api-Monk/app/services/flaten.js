b8098828b0f3f870ea4e0c39cdc70fc6d8ec64a5
Saurabh Bansal <saurabh.bansal@marinsoftware.com>
2017-08-15 17:10:27 -0700

    more changes and test cases

--- Api-Monk/app/services/flaten.js
+++ Api-Monk/app/services/flaten.js
@@ -5,30 +5,59 @@ const _ = require('underscore')
 const json2csv = require('json2csv');
 const is = require('is');
 
-/**
- * Function to parse json into csv
- */
-function flatJson(){
 
-}
 
 /**
- * Parse field names out of json
+ * Parse field names and unwind objects out of json
+ * objects that are array , and items of array are objects need to be in this unwinded array
  */
-function parseFields(data){
+function parseProperties(data){
     const fields = []
+    const unwind = []
+
     //{"coord":{"lon":-0.13,"lat":51.51},"weather":[{"id":300,"main":"Drizzle","description":"light intensity drizzle","icon":"09d"}],"base":"stations","main":{"temp":280.32,"pressure":1012,"humidity":81,"temp_min":279.15,"temp_max":281.15},"visibility":10000,"wind":{"speed":4.1,"deg":80},"clouds":{"all":90},"dt":1485789600,"sys":{"type":1,"id":5091,"message":0.0103,"country":"GB","sunrise":1485762037,"sunset":1485794875},"id":2643743,"name":"London","cod":200}
+
+    // case when data is an object
     for  (let key in data) {
+
+        // check if property is an object
         if (is.object(data[key])) {
-            parseObjectType(key,data[key],fields)
+            parseObjectType(key,data[key],fields,unwind)
         }
-        // only check 1st item of array assuming strcuture is same
-        if (is.array(data[key])) {
-           data[key][0]
+
+        // if property is an array
+        else if (is.array(data[key])) {
+            parseArrayType(key,data[key],fields,unwind)
+        }
+
+        else  {
+
+            addToFields(key,fields)
         }
     }
-    return fields
+
+    // case when data is an array
+    // loop over top 100 items
+
+    return {
+        fields,unwind
+    }
 }
+
+function parseArrayType(parentKey,array,fields,unwind){
+    "use strict";
+    array.forEach(function(item){
+        if (is.object(item)) {
+            addToFields(parentKey,unwind)
+            parseObjectType(parentKey,item,fields,unwind)
+        } else if (is.array(item)) {
+
+        } else {
+            addToFields(parentKey,fields)
+        }
+    })
+}
+
 
 /**
  * Recursive function to loop over objects
@@ -36,15 +65,29 @@ function parseFields(data){
  * @param object
  * @param fields
  */
-function parseObjectType(parentKey,object,fields){
+function parseObjectType(parentKey,object,fields,unwind){
     for (let key in object){
         if (is.object(object[key])) {
-            parseObjectType(keyName([parentKey,key]),object[key],fields)
+            parseObjectType(keyName([parentKey,key]),object[key],fields,unwind)
         } else {
-            fields.push(keyName([parentKey,key]))
+            addToFields(keyName([parentKey,key]),fields)
         }
     }
     return fields
+}
+
+/**
+ * Add key to field if doesn't exists already
+ * @param keyName
+ * @param fields
+ */
+function addToFields(keyName,fields){
+    "use strict";
+    // add if not already exists
+    let field = _.find(fields,function(f){return f === keyName})
+    if (!field){
+        fields.push(keyName)
+    }
 }
 
 /**
@@ -60,8 +103,15 @@ function keyName(keys) {
     return nonEmpty.join('.')
 }
 
-parseFields()
+/**
+ * Function to parse json into csv
+ */
+function parseJson(data){
+    let properties = parseProperties(data)
+    let csv = json2csv({ data: data, fields: properties.fields, unwind:properties.unwind });
+    return csv
+}
 
 module.exports  = {
-    flatJson
+    parseJson
 }
--- Api-Monk/app/services/flaten.test.js
+++ Api-Monk/app/services/flaten.test.js
@@ -5,12 +5,15 @@ const flatenService = rewire('./flaten.js');
 describe('flaten.Service',function(){
     let parseObjectType ;
     let keyName ;
-    let parseFields;
+    let parseProperties;
+    let addToFields;
 
     before(function(){
         parseObjectType = flatenService.__get__('parseObjectType')
         keyName = flatenService.__get__('keyName')
-        parseFields = flatenService.__get__('parseFields')
+        parseProperties = flatenService.__get__('parseProperties')
+        addToFields = flatenService.__get__('addToFields')
+
     })
 
     describe('keyName',function(){
@@ -45,21 +48,109 @@ describe('flaten.Service',function(){
 
     })
 
-    describe('parseFields',function(){
+    describe('addToFields',function(){
+        "use strict";
 
-        it('should return two fields from object',function(){
+        it("should add key if not exists",function(){
+            let fields = []
+            let result = addToFields("key1",fields)
+            expect(fields.length).to.equal(1)
+        })
+
+        it("should not add key if it already exists",function(){
+            let fields = ['key1']
+            let result = addToFields("key1",fields)
+            expect(fields.length).to.equal(1)
+        })
+
+    })
+
+    describe('parseProperties',function(){
+
+        it('case 1 -> data is an simple object with two properties',function(){
             const source = {
                 "coord": {"lon": -0.13, "lat": 51.51}
             }
-            const result = parseFields(source)
-            expect(result).to.deep.equal([ 'coord.lon', 'coord.lat' ])
+            const result = parseProperties(source)
+            expect(result.fields).to.deep.equal([ 'coord.lon', 'coord.lat' ])
+            expect(result.unwind.length).to.equal(0)
         })
 
-        it('case',function(){
+        it('case 2 -> data is an object with one property an array of objects, only one item',function(){
             const source  = {"coord":{"lon":-0.13,"lat":51.51},"weather":[{"id":300,"main":"Drizzle","description":"light intensity drizzle","icon":"09d"}]}
-            console.log(parseFields(source))
+            const result = parseProperties(source)
+            expect(result.fields).to.deep.equal([ 'coord.lon',
+                    'coord.lat',
+                    'weather.id',
+                    'weather.main',
+                    'weather.description',
+                    'weather.icon' ])
+            expect(result.unwind).to.deep.equal(['weather'])
         })
 
+        it('case 3 -> data is an object with one property an array of objects, two same objects',function(){
+            const source  = {"coord":{"lon":-0.13,"lat":51.51},"weather":[
+                {"id":300,"main":"Drizzle","description":"light intensity drizzle","icon":"09d"},
+                {"id":300,"main":"Drizzle","description":"light intensity drizzle","icon":"09d"}
+                ]}
+            const result = parseProperties(source)
+            expect(result.fields).to.deep.equal([ 'coord.lon',
+                'coord.lat',
+                'weather.id',
+                'weather.main',
+                'weather.description',
+                'weather.icon' ])
+            expect(result.unwind).to.deep.equal(['weather'])
+        })
+
+        it('case 4 -> data is an object with one property an array of objects, two objects, one object having extra property',function(){
+            const source  = {"coord":{"lon":-0.13,"lat":51.51},"weather":[
+                {"id":300,"main":"Drizzle","description":"light intensity drizzle","icon":"09d"},
+                {"id":300,"main":"Drizzle","description":"light intensity drizzle","icon":"09d","extraProp":"value"}
+                ]}
+            const result = parseProperties(source)
+            expect(result.fields).to.deep.equal([ 'coord.lon',
+                'coord.lat',
+                'weather.id',
+                'weather.main',
+                'weather.description',
+                'weather.icon',
+                'weather.extraProp'])
+            expect(result.unwind).to.deep.equal(['weather'])
+        })
+
+        it('case 5 -> data is an object with one property an array, array of int or string, not objects',function(){
+            "use strict";
+            const source = {
+                "coord": {"lon": -0.13, "lat": 51.51},"weather":[1,2,3]
+            }
+            const result = parseProperties(source)
+            expect(result.fields).to.deep.equal([ 'coord.lon', 'coord.lat', 'weather' ])
+            expect(result.unwind.length).to.equal(0)
+        })
+
+        xit('case 6 -> data is an object with one property an array of objects, that property has another property that is an array',function(){
+            const source  = {"coord":{"lon":-0.13,"lat":51.51},"weather":[{"id":300,"main":"Drizzle","description":"light intensity drizzle","icon":"09d",values:[1,2,3]}]}
+            const result = parseProperties(source)
+            expect(result.fields).to.deep.equal([ 'coord.lon',
+                'coord.lat',
+                'weather.id',
+                'weather.main',
+                'weather.description',
+                'weather.icon' ])
+            expect(result.unwind).to.deep.equal(['weather'])
+        })
+    })
+
+    describe('parseJson',function(){
+        "use strict";
+        it('case 1 -> data is an simple object with two properties',function(){
+            const source = {
+                "coord": {"lon": -0.13, "lat": 51.51}
+            }
+            const result = flatenService.parseJson(source)
+            console.log(result)
+        })
 
     })
 
