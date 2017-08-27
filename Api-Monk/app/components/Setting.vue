34039eecea8fe6367cb754e9057e727487de188d
Saurabh Bansal <saurabh.bansal@marinsoftware.com>
2017-08-27 09:48:29 -0700

    added settings.vue

---
+++ Api-Monk/app/components/Setting.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <bootstrap-modal ref="theModal" :needHeader="false" :needFooter="false" size="large">
+            <div slot="title">
+                Your title here
+            </div>
+            <div slot="body">
+                Your body here
+            </div>
+            <div slot="footer">
+                Your footer here
+            </div>
+        </bootstrap-modal>
+        ...
+    </div>
+</template>
+
+<script>
+    const bootstrapModal = require('vue2-bootstrap-modal')
+    require('bootstrap-loader')
+    export default {
+        components :{
+            'bootstrap-modal': bootstrapModal
+        }
+
+    }
+
+</script>
